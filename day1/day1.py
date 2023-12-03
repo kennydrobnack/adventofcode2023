1f95f3af67806a3474b20f157727b46375bf9e93
Kenny Drobnack <kennydrobnack@gmail.com>
2023-12-02 23:48:36 -0500

    Finished part 2

--- day1/day1.py
+++ day1/day1.py
@@ -3,17 +3,60 @@ import re
 #to form a single two-digit number.
 
 def first_plus_last_digit(code):
-    m = re.search(r"\d", code)
-    rev = code[::-1]
-    r = re.search(r"\d", rev)
-    first_digit = code[m.start()]
-    last_digit = rev[r.start()]
+    matches = re.findall(r"\d", code)
+    first_digit = matches[0]
+    last_digit = matches[-1]
     return int(f"{first_digit}{last_digit}")
+
+def part_two_first_plus_last_digit(code):
+    pattern = "one|two|three|four|five|six|seven|eight|nine"
+    matches = re.findall(f"{pattern}|\d", code)
+    first_digit = written_number_to_digit(matches[0])
+    rev_pattern = f"{pattern[::-1]}|\d"
+    rev_matches = re.findall(rev_pattern, code[::-1])
+    try:
+        last_digit = rev_matches[0]
+        if (len(last_digit) > 1):
+            last_digit = written_number_to_digit(last_digit[::-1])
+    except Exception as ex:
+        print(f"Failed to find last digit of {code} with error {ex}")
+    return int(f"{first_digit}{last_digit}")    
+
+def written_number_to_digit(code):
+    p = re.compile('one')
+    code = p.sub("1", code)
+    p = re.compile('two')
+    code = p.sub("2", code)
+    p = re.compile('three')
+    code = p.sub("3", code)
+    p = re.compile('four')
+    code = p.sub("4", code)
+    p = re.compile('five')
+    code = p.sub("5", code)
+    p = re.compile('six')
+    code = p.sub("6", code)
+    p = re.compile('seven')
+    code = p.sub("7", code)
+    p = re.compile('eight')
+    code = p.sub("8", code)
+    p = re.compile('nine')
+    code = p.sub("9", code)
+    return code
 
 def add_first_and_last_digits_of_list(list_of_codes):
     sum = 0
     for code in list_of_codes:
-        sum = sum + first_plus_last_digit(code)
+        updated_code = written_number_to_digit(code)
+        sum = sum + first_plus_last_digit(updated_code)
+    return sum
+
+def part_two_add_first_and_last_digits_of_list(list_of_codes):
+    sum = 0
+    try:
+        for code in list_of_codes:
+            sum = sum + part_two_first_plus_last_digit(code)
+    except:
+        print("Something went wrong!")
     return sum
 
 def main():
@@ -30,6 +73,19 @@ def main():
     data_into_list = data.split("\n")
     result = add_first_and_last_digits_of_list(data_into_list)
     print(f"Results: {result}")
+    part_two_test_codes = [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen"
+    ]
+    result = part_two_add_first_and_last_digits_of_list(part_two_test_codes)
+    print(f"Results: {result}")
+    result = part_two_add_first_and_last_digits_of_list(data_into_list)
+    print(f"Result: {result}")
 
 if __name__ == "__main__":
     main()
