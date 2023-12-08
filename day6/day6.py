2fd2f6df7b0821a14e8671b4dfe366323919ea7f
Kenny Drobnack <kennydrobnack@gmail.com>
2023-12-07 20:51:07 -0500

    Got some code for part 2 and it takes forever to run

--- day6/day6.py
+++ day6/day6.py
@@ -4,6 +4,7 @@ import re
 
 
 def find_possible_winning_races(race_time, record_distance):
+    print(f"Calculating ways to win a race time {race_time} with record distance {record_distance}")
     winning_times = [hold_time for hold_time in range(1, record_distance - 1) 
                      if calculate_race_distance(hold_time, race_time) > record_distance]
     print(f"Winning times for race of time {race_time} previous record {record_distance}:\n {winning_times}")
@@ -13,6 +14,10 @@ def calculate_race_distance(button_hold_time, total_time):
     speed = button_hold_time
     distance = speed * (total_time - button_hold_time)
     return distance
+
+def read_lines_bad_kerning(line):
+    number = re.sub("\D", "", line)
+    return int(number)
 
 def read_lines(line):
     numbers = re.split("\s+", line)
@@ -32,6 +37,11 @@ def multiply_ways_to_win_races(lines):
 
     return results
 
+def part_two_results(lines):
+    time = read_lines_bad_kerning(lines[0])
+    distance = read_lines_bad_kerning(lines[1])
+    return find_possible_winning_races(time, distance)
+
 def main():
     print("Running")
     my_file = open("./day6/day6_data.txt", "r") 
@@ -39,6 +49,9 @@ def main():
     data_into_list = data.split("\n")
     result = multiply_ways_to_win_races(data_into_list)
     print(f"Final result: {result}")
+    print("Starting part two:")
+    results2 = part_two_results(data_into_list)
+    print(f"Final part two results: {results2}")
 
 if __name__ == '__main__':
     main()
--- day6/test_day6.py
+++ day6/test_day6.py
@@ -1,4 +1,4 @@
-from day6 import multiply_ways_to_win_races, calculate_race_distance, find_possible_winning_races
+from day6 import multiply_ways_to_win_races, calculate_race_distance, find_possible_winning_races, read_lines_bad_kerning, part_two_results
 
 def test_multiply_ways_to_win():
     sample_line1 = "Time:      7  15   30"
@@ -14,3 +14,12 @@ def test_race_distance():
     hold_time = 5
     total_time = 7
     assert calculate_race_distance(hold_time, total_time) == 10
+
+def test_readlines_bad_kerning():
+    line = "TIME: 12 34 56"
+    assert read_lines_bad_kerning(line) == 123456
+
+def test_part_two_results():
+    sample_line1 = "Time:      7  15   30"
+    sample_line2 = "Distance:  9  40  200"
+    assert part_two_results([sample_line1, sample_line2]) == 71503
